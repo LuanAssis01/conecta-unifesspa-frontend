05d06cf6b07eb99a604479d1eb1a6d7d6e3f28d8
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-12-13 18:52:01 -0300

    chore(types): organize and add application types

--- src/types/courseTypes.ts
+++
@@ -1,7 +0,0 @@
-export type Course = {
-    id: number;
-    name: string;
-    code: string;
-    department: string;
-    students: number;
-}
--- src/types/statisticsTypes.ts
+++
@@ -1,8 +0,0 @@
-export type Statistics = {
-    totalProjects: number;
-    activeProjects: number;
-    totalUsers: number;
-    pendingApprovals: number;
-    totalCourses: number;
-    projectsThisMonth: number;
-}
